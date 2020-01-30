f7905dc4df0748914f908dc580c2b4cd60530e6d
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-01-30 11:45:33 -0500

    fix dependencies

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express();
 
-const corsOptions: cors.CorsOptions = {
+const corsOptions = {
   origin: true,
   optionsSuccessStatus: 200
 };
