23cb6cd0684e51828527c96d6bab51a90dde96b5
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-01-30 14:49:28 -0600

    hook up express ; fix validator

--- src/api.js
+++ src/api.js
@@ -1,64 +1,66 @@
 const validator = require('./validator');
 const Joi = require('@hapi/joi');
 
-app.get(
-  '/api/checkRegistrationCode',
-  validator(Joi.object({ code: Joi.string().required() })),
-  (req, res) => {
-    const { code } = req.body;
-    // TODO
-    const responseBody = {
-      success: true,
-      volunteerType: 'volunteerDistributor' | 'ambassadorVolunteer',
-      // for volunteerDistributor users:
-      accountId: '', // representing one or more teams of volunteers
-      isIndividualDistributor: true | false,
-      teamCoordinators: {
-        name: '',
+module.exports = function(app) {
+  app.get(
+    '/api/checkRegistrationCode',
+    validator(Joi.object({ code: Joi.string().required() })),
+    (req, res) => {
+      const { code } = req.body;
+      // TODO
+      const responseBody = {
+        success: true,
+        volunteerType: 'volunteerDistributor' | 'ambassadorVolunteer',
+        // for volunteerDistributor users:
+        accountId: '', // representing one or more teams of volunteers
+        isIndividualDistributor: true | false,
+        teamCoordinators: {
+          name: '',
+          salesforceId: ''
+          // }[] // ???
+        }
+      };
+      res.send(responseBody);
+    }
+  );
+
+  app.get(
+    '/api/contactSearch',
+    validator(
+      Joi.object({
+        email: Joi.string()
+          .email()
+          .required(),
+        phone: Joi.string().required()
+      })
+    ),
+    (req, res) => {
+      const { email, phone } = req.body;
+      // TODO
+      const responseBody = {
         salesforceId: ''
-        // }[] // ???
-      }
-    };
-    res.send(responseBody);
-  }
-);
+      };
+      res.send(responseBody);
+    }
+  );
 
-app.get(
-  '/api/contactSearch',
-  validator(
-    Joi.object({
-      email: Joi.string()
-        .email()
-        .required(),
-      phone: Joi.string().required()
-    })
-  ),
-  (req, res) => {
-    const { email, phone } = req.body;
-    // TODO
-    const responseBody = {
-      salesforceId: ''
-    };
-    res.send(responseBody);
-  }
-);
-
-app.get(
-  '/api/createFeedback',
-  validator(
-    Joi.object({
-      email: Joi.string()
-        .email()
-        .required(),
-      phone: Joi.string().required()
-    })
-  ),
-  (req, res) => {
-    const { email, phone } = req.body;
-    // TODO
-    const responseBody = {
-      salesforceId: ''
-    };
-    res.send(responseBody);
-  }
-);
+  app.get(
+    '/api/createFeedback',
+    validator(
+      Joi.object({
+        email: Joi.string()
+          .email()
+          .required(),
+        phone: Joi.string().required()
+      })
+    ),
+    (req, res) => {
+      const { email, phone } = req.body;
+      // TODO
+      const responseBody = {
+        salesforceId: ''
+      };
+      res.send(responseBody);
+    }
+  );
+};
