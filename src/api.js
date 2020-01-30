acb48f56098bb4ed3c5cd1478319da399241eaf2
priyanka <kpriyanka1102@gmail.com>
2020-01-30 14:54:23 -0600

    created routes for api methods

--- src/api.js
+++ src/api.js
@@ -3,9 +3,13 @@ const Joi = require('@hapi/joi');
 
 app.get(
   '/api/checkRegistrationCode',
-  validator(Joi.object({ code: Joi.string().required() })),
+  validator(Joi.object({
+    code: Joi.string().required()
+  })),
   (req, res) => {
-    const { code } = req.body;
+    const {
+      code
+    } = req.body;
     // TODO
     const responseBody = {
       success: true,
@@ -34,7 +38,10 @@ app.get(
     })
   ),
   (req, res) => {
-    const { email, phone } = req.body;
+    const {
+      email,
+      phone
+    } = req.body;
     // TODO
     const responseBody = {
       salesforceId: ''
@@ -54,11 +61,108 @@ app.get(
     })
   ),
   (req, res) => {
-    const { email, phone } = req.body;
+    const {
+      email,
+      phone
+    } = req.body;
     // TODO
     const responseBody = {
       salesforceId: ''
     };
     res.send(responseBody);
+  }
+);
+
+app.post(
+  '/api/deleteOutreachLocation',
+  validator(
+    Joi.object({
+      firebaseIdToken: Joi.string()
+        .firebaseIdToken()
+        .required(),
+      outreachLocationId: Joi.string()
+        .required()
+    }), true
+  ),
+  (req, res) => {
+
+    const {
+      firebaseIdToken,
+      outreachLocationId
+    } = req.body;
+
+    //TODO
+    const responseBody = {
+      success: true
+    };
+
+    res.send(responseBody)
+  }
+);
+
+app.post(
+  '/api/updateNotificationPreferences',
+  validator(
+    Joi.object({
+      firebaseIdToken: Joi.string()
+        .firebaseIdToken()
+        .required(),
+      fcmToken: Joi.string()
+        .fcmToken()
+        .required(),
+      language: Joi.string(),
+      preEventSurveyReminderEnabled: Joi.string(),
+      reportReminderEnabled: Joi.string(),
+      upcomingEventsReminderEnabled: Joi.string()
+
+    }), true
+  ),
+  (req, res) => {
+
+    const {
+      firebaseIdToken,
+      fcmToken,
+      language,
+      preEventSurveyReminderEnabled,
+      reportReminderEnabled,
+      upcomingEventsReminderEnabled
+
+    } = req.body;
+
+    //TODO
+    const responseBody = {
+      success: true
+    };
+
+    res.send(responseBody)
+  }
+
+);
+app.post(
+  '/api/unregisterFcmToken',
+  validator(
+    Joi.object({
+      firebaseIdToken: Joi.string()
+        .firebaseIdToken()
+        .required(),
+      fcmToken: Joi.string()
+        .fcmToken()
+        .required()
+    }), true
+  ),
+  (req, res) => {
+
+    const {
+      firebaseIdToken,
+      fcmToken
+
+    } = req.body;
+
+    //TODO
+    const responseBody = {
+      success: true
+    };
+
+    res.send(responseBody)
   }
 );
